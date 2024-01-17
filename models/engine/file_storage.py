fd8687994f2f63241d2e2ec2ec78b7e58fef5574
Nii-101 <noelamptey101@gmail.com>
2024-01-17 20:23:07 +0000

    Added the delete instance method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,15 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+        """Deletes obj from __objects"""
+        if obj == None:
+            pass
+        else:
+            key = f"{type(obj).__name__}.{obj.id}"
+            if key in self__objects:
+                del self.__objects[key]
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.amenity import Amenity
