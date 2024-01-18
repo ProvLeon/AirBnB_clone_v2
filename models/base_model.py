6aab935c0681e32e6f03bf9357a2c9cb68610b0f
ProvLeon <providence.leonard@gmail.com>
2024-01-18 03:28:50 +0000

    modified delete, to_dict and save methods in file_storage.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.orm import declarative_base
 from datetime import datetime
 
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.orm import declarative_base
+
+from models import storage
 
 Base = declarative_base()
 
@@ -38,24 +40,19 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = dict(self.__dict__)
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            del dictionary['_sa_instance_state']
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
-        """Deletes everything"""
-        from models import storage
-        storage.delete()
+        """Deletes the current instance from the storage"""
+        storage.delete(self)
