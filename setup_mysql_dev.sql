16a1669e5538a3fd9bbc894fd5744be465815b8c
ProvLeon <providence.leonard@gmail.com>
2024-01-18 07:23:15 +0000

    refactored the queries for task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,15 +4,19 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER 
   IF NOT EXISTS 'hbnb_dev'@'localhost' 
   IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant usage on all databases to the user
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
+-- Grant all privileges on hbnb_dev_db to the user
+GRANT ALL PRIVILEGES 
+  ON `hbnb_dev_db`.* 
+  TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to the user
 GRANT SELECT 
   ON `performance_schema`.* 
   TO 'hbnb_dev'@'localhost'
-  IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES 
-  ON `hbnb_dev_db`.* 
-  TO 'hbnb_dev'@'localhost'
-  IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- FLUSH applies all the privilege changes instantly
 FLUSH PRIVILEGES;
