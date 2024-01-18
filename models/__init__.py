e87d866001528c9aa194e4d577d07962f9e77ba3
Nii-101 <noelamptey101@gmail.com>
2024-01-18 02:08:21 +0000

    Finished with task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+import os
 
+
+storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
+
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage as storage
+
+else:
+    storage = FileStorage()
 
 storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -5,4 +5,17 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column("name", String(128), nullable=False)
+
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
+
+
+@property
+def cities(self):
+    """getter attributes for cities in FileStorage"""
+    from models import storage
+    city_instances = storage.all('City')
+    return [city for city in city_instances.values()
+            if city.state_id == self.id]
