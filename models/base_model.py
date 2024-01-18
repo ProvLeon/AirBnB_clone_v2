2dfcd3fa73f13542c392b4eba2c3e68cb5f15625
Nii-101 <noelamptey101@gmail.com>
2024-01-18 02:38:52 +0000

    debugged it a bit

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid  
-from datetime import datetime
-from sqlalchemy import Column, String, Datetime
+import uuid
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 
 Base = declarative_base()
@@ -16,7 +16,7 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
+        """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -51,11 +51,11 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '__sa_instance_state' in dictionary.keys():
-            del dictionary['__sa_instance_state']
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
-        """Deletes Everything"""
+        """Deletes everything"""
         from models import storage
         storage.delete()
