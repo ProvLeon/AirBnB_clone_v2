f2828b27b3a7e9bf33e1b23e980ff06bc73dc852
Nii-101 <noelamptey101@gmail.com>
2024-01-18 02:15:21 +0000

    Debugged state.py

--- models/state.py
+++ models/state.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 class State(BaseModel):
     """ State class """
     __tablename__ = "states"
-    name = Column("name", String(128), nullable=False)
+    if storage_type == "db":
+        name = Column("name", String(128), nullable=False)
+        cities = relationship('City', backref="state", cascade="all, delete")
 
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
-
-
-@property
-def cities(self):
-    """getter attributes for cities in FileStorage"""
-    from models import storage
-    city_instances = storage.all('City')
-    return [city for city in city_instances.values()
-            if city.state_id == self.id]
+    else:
+        name = ""
