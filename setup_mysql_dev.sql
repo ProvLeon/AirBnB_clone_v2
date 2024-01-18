b27d214bd0aae9e7b599a88be8c56ef027ac3615
ProvLeon <providence.leonard@gmail.com>
2024-01-18 07:07:38 +0000

    completed task 3 error

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,17 @@
 -- Prepares a MySQL server
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+CREATE USER 
+  IF NOT EXISTS 'hbnb_dev'@'localhost' 
+  IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES 
+  ON `hbnb_dev_db`.* 
+  TO 'hbnb_dev'@'localhost'
+  IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT 
+  ON `performance_schema`.* 
+  TO 'hbnb_dev'@'localhost'
+  IDENTIFIED BY 'hbnb_dev_pwd';
+
 -- FLUSH applies all the privilege changes instantly
 FLUSH PRIVILEGES;
