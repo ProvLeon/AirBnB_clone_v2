88e3db7e002ad24a3b49fe515f9244440944927b
ProvLeon <providence.leonard@gmail.com>
2024-02-28 20:16:46 +0000

    added server address

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.157.134.178', '54.175.136.136']
 
 
 def do_pack():
